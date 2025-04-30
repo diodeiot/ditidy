6c737ce6c70b19a031ba67ebf586188083860098
kadir sevil <kadirsevil@MacBook-Pro.home>
2025-04-30 11:51:58 +0300

    added default config file selection feature
    when the --config-file option is not specified,
    the program tries to use a config file
    in the working directory

--- src/ditidy/cli.py
+++ src/ditidy/cli.py
@@ -1,16 +1,26 @@
 import os
 from importlib.metadata import version
+from pathlib import Path
 
 import click
 
 from ditidy.checks.checks import checks
 from ditidy.config import parse
+from ditidy.error import on_error
 
 
 @click.command()
 @click.version_option(version("ditidy"), message="%(version)s")
-@click.option("--config-file", required=True, type=str, help="Path of the config file")
+@click.option("--config-file", type=str, help="Path of the config file")
 def cli(config_file):
+    # pick default config file
+    if config_file is None:
+        p = Path.cwd()/"ditidy.yaml"
+        if p.exists():
+            config_file = str(p)
+        else:
+            on_error("config file could not found")
+
     config_file_dir = os.path.dirname(config_file)
     if os.path.isabs(config_file):
         root_dir = config_file_dir
